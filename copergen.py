55d23776beb0ae87213a86c12f73ecc03d5ed165
Said Polanco-Martagón <spolancom@upv.edu.mx>
2018-05-14 18:13:12 -0500

    avance minimo

--- copergen.py
+++ copergen.py
@@ -1,19 +1,27 @@
 from numpy import random, ceil
 from copy import deepcopy
+from tindividuo import TIndividuo
 
 
 class COperGen:
 
+    def __init__(self, prob_cruce=0.6, prob_mut=0.01):
+        self.prob_cruce = prob_cruce
+        self.prob_mut = prob_mut
+
     def obtenPoblacion(self, pob):
         self.pob = pob.pob
         self.tamPob = pob.tamPob
+        self.lcrom = pob.lcrom
+        self.x_min = pob.x_min
+        self.x_max = pob.x_max
 
     def seleccion(self):
         sel_super = []
         TAM_ELITE = int(ceil(self.tamPob * 2 / 100))
 
         # Se selecciona la elite
-        sel_elite = [0 for i in range(0,TAM_ELITE)]
+        sel_elite = [0 for i in range(0, TAM_ELITE)]
         for i in range(0, self.tamPob):
             self.pob[i].elite = False
             j = 0
@@ -29,9 +37,8 @@ class COperGen:
         # Se seleccionan tam_pob individuos para reproducirse
         # se generan números aleatorios entre 0 y 1,
         # seleccionan individuos de acueredo con su puntuación acumulada
-
         for i in range(0, self.tamPob):
-            if self.pob[i].elite: # ELITISMO
+            if self.pob[i].elite:   # ELITISMO
                 sel_super.append(i)
                 print("elite: {}".format(i))
             else:
@@ -47,5 +54,44 @@ class COperGen:
 
         # se genera la población intermedia
         pob_aux = [deepcopy(self.pob[i]) for i in sel_super]
-        for i in pob_aux:
-            print(pob_aux.genes)
+        self.pob = []  # limpiamos la poblacion original
+        self.pob = [deepcopy(i) for i in pob_aux]
+        del pob_aux  # liberamos espacio en memoria
+        print([i.aptitud for i in self.pob])
+        del sel_super
+        del sel_elite
+
+    def reproduccion(self):
+        num_sel_cruce = 0  # num de individuos seleccionados a cruzar
+        sel_cruce = []
+        # Se eligen los individuos a cruzar
+        for i in range(0, self.tamPob):
+            # se generan tam_pob numeros aleatorios a_i en [0, 1)
+            # se eligen para el cruce los individuos de
+            # las posiciones i con a_i < prob_cruce
+            if random.random_sample() < self.prob_cruce:
+                sel_cruce.append(i)
+                num_sel_cruce += 1
+
+        # el número de seleccionados se hace par
+        if num_sel_cruce % 2 != 0:
+            num_sel_cruce -= 1
+
+        # Se cruzan los individuos elegidos en un punto al azar
+        punto_cruce = int(random.random_integers(0, self.lcrom-2))
+        print("Punto de cruce {}".format(punto_cruce))
+
+        for i in range(0, num_sel_cruce, 2):
+            h1 = TIndividuo(self.lcrom, self.x_min, self.x_max, self.pob[
+                sel_cruce[i]].genes[0:punto_cruce] + self.pob[
+                    sel_cruce[i+1]].genes[punto_cruce:self.lcrom])
+            h2 = TIndividuo(self.lcrom, self.x_min, self.x_max, self.pob[
+                sel_cruce[i+1]].genes[0:punto_cruce] + self.pob[
+                    sel_cruce[i]].genes[punto_cruce:self.lcrom])
+
+            # print(self.pob[sel_cruce[i]].getAptitud(), " vs ", h1.getAptitud())
+            # print(self.pob[
+            #    sel_cruce[i+1]].getAptitud(), " vs ", h2.getAptitud())
+
+            if h1.getAptitud() < self.pob[sel_cruce[i]].getAptitud() or not self.pob[sel_cruce[i]].elite:
+                print("aaqui me quedé")
--- ga.py
+++ ga.py
@@ -2,6 +2,8 @@ import argparse
 from math import ceil, log
 from tpoblacion import TPoblacion
 from testFuncObjet import Display
+from copergen import COperGen
+
 
 # Se obtienen los parámetros por medio de la línea de comandos
 parser = argparse.ArgumentParser(description="Algoritmo genético simple")
@@ -20,6 +22,9 @@ args = parser.parse_args()   # se obtiene el objeto
 # Calculamos el largo del cromosoma
 lcrom = ceil(log(1 + ((args.x_max - args.x_min)/args.tol), 2))
 
+# inicializamos los objetos auxiliares
+og = COperGen()
+
 # 1.- Se genera la población inicial. (cada individuo se auto evalua)
 pob = TPoblacion(args.tamPob, lcrom, args.x_min, args.x_max)
 
@@ -32,4 +37,7 @@ pob.evaluacion()
 
 # Bucle de evolución
 for i in range(0, args.gen):
-
+    og.obtenPoblacion(pob.clone())
+    # 3.- Seleccion de los indiviudos a ser reproducidos
+    og.seleccion()
+    og.reproduccion()
